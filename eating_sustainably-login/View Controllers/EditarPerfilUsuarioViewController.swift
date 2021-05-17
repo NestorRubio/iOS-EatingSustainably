7f28779d81c940106d1a8c635a218392231caeb6
Enrique Elizondo <quiqueev@gmail.com>
2021-05-16 23:26:53 -0500

    Se modifico el perfil de usuario, se agrego el view de mapa usuario y el view para editar usuarios

---
+++ eating_sustainably-login/View Controllers/EditarPerfilUsuarioViewController.swift
@@ -0,0 +1,29 @@
+//
+//  EditarPerfilUsuarioViewController.swift
+//  eating_sustainably-login
+//
+//  Created by Enrique Elizondo on 5/16/21.
+//
+
+import UIKit
+
+class EditarPerfilUsuarioViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
