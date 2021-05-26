f95c0f52a40c02e579c200a29ce750509ac9de05
NestorRubio <nestorulo29@gmail.com>
2021-05-26 15:33:49 -0700

    Cambio de contraseña y borrar cuenta de usuario normal

--- eating_sustainably-login/ModificarPasswordViewController.swift
+++ eating_sustainably-login/ModificarPasswordViewController.swift
@@ -6,12 +6,30 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ModificarPasswordViewController: UIViewController {
     
     var usuarioVerPerfil : Usuario!
     var ver : Bool = false
-
+    
+    @IBOutlet weak var lbContraseñaAct: UILabel!
+    @IBOutlet weak var tfContraseñaAct: UITextField!
+    
+    
+    @IBOutlet weak var lbNewPassword: UILabel!
+    @IBOutlet weak var tfNewPassword: UITextField!
+    
+    
+    @IBOutlet weak var lbNewPasswordConf: UILabel!
+    @IBOutlet weak var tfNewPasswordConf: UITextField!
+    
+    
+    @IBOutlet weak var lbEmail: UILabel!
+    @IBOutlet weak var tfEmail: UITextField!
+    
+    @IBOutlet weak var bttnChangePassword: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Modificar contraseña"
@@ -19,7 +37,40 @@ class ModificarPasswordViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    func changePassword(email : String, currentPassword : String, newPassword : String, completion: @escaping (Error) -> Void){
+        
+        let credential = EmailAuthProvider.credential(withEmail: email, password: currentPassword)
+        
+        Auth.auth().currentUser?.reauthenticate(with: credential, completion: {(result, error) in
+            if let error = error{
+                completion(error)
+            }else{
+                Auth.auth().currentUser?.updatePassword(to: newPassword, completion: {(error) in
+                    print("error")
+                })
+            }
+        })
+    }
+    
+    @IBAction func cambiarContraseña(_ sender: UIButton) {
+  
+        if(tfNewPassword.text! == tfNewPasswordConf.text!){
+            
+            self.changePassword(email: tfEmail.text!, currentPassword: tfContraseñaAct.text!, newPassword: tfNewPassword.text!, completion: {(error) in
+                if error == nil{
+        
+                    let alertController = UIAlertController(title: "Error", message: "No se pudo actualizar la contraseña. Intentelo de nuevo", preferredStyle: .alert)
+                    alertController.addAction(UIAlertAction(title: "Entendido", style: .default))
+                    self.present(alertController, animated: true, completion: nil)
+                }else{
+                    let alertController = UIAlertController(title: "Exito!", message: "Contraseña actualizada correctamente", preferredStyle: .alert)
+                    alertController.addAction(UIAlertAction(title: "Entendido", style: .default))
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            })
+        }
+    }
+    
     /*
     // MARK: - Navigation
 
--- eating_sustainably-login/View Controllers/EliminarViewController.swift
+++ eating_sustainably-login/View Controllers/EliminarViewController.swift
@@ -6,20 +6,77 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class EliminarViewController: UIViewController {
     
     var usuarioVerPerfil : Usuario!
     var ver : Bool = false
-
+    
+    @IBOutlet weak var lbCorreo: UILabel!
+    @IBOutlet weak var tfCorreo: UITextField!
+    @IBOutlet weak var lbPassword: UILabel!
+    @IBOutlet weak var tfPassword: UITextField!
+    @IBOutlet weak var bttnDelAccount: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Eliminar usuario"
 
         // Do any additional setup after loading the view.
+        let alertController = UIAlertController(title: "Aviso", message: "Una vez eliminada una cuenta, esta no podra ser recuperada. ", preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "Entendido", style: .default))
+        self.present(alertController, animated: true, completion: nil)
     }
     
+    func cerrarSesion(alert : UIAlertAction!){
+        
+        let portada = self.storyboard?.instantiateViewController(identifier: Constantes.Storyboard.portadaViewController) as? UINavigationController
+        self.view.window?.rootViewController = portada
+        self.view.window?.makeKeyAndVisible()
+            //mostramos mensajes de confirmacion y vamos a portada
+    }
+    
+    func deleteAccount(email : String, currentPassword : String, completion: @escaping (Error) -> Void){
+        
+        let credential = EmailAuthProvider.credential(withEmail: email, password: currentPassword)
+        
+        Auth.auth().currentUser?.reauthenticate(with: credential, completion: {(result, error) in
+            if let error = error{
+                let alertController = UIAlertController(title: "Error", message: "Credenciales incorrectas", preferredStyle: .alert)
+                alertController.addAction(UIAlertAction(title: "Entendido", style: .default))
+                self.present(alertController, animated: true, completion: nil)
+                completion(error)
+            }else{
+                let user = Auth.auth().currentUser
+                user?.delete { error in
+                    if error != nil{
+                        let alertController = UIAlertController(title: "Error", message: "No se pudo eliminar la cuenta. Intentelo de nuevo", preferredStyle: .alert)
+                        alertController.addAction(UIAlertAction(title: "Entendido", style: .default))
+                        self.present(alertController, animated: true, completion: nil)
+                    }else{
+                        let alertController = UIAlertController(title: "Exito!", message: "La cuenta ha sido eliminada correctamente", preferredStyle: .alert)
+                        alertController.addAction(UIAlertAction(title: "Entendido", style: .default, handler: self.cerrarSesion ))
+                        self.present(alertController, animated: true, completion: nil)
 
+                    }
+                }
+            }
+        })
+    }
+    
+    
+    @IBAction func borrarCuenta(_ sender: UIButton) {
+        deleteAccount(email: tfCorreo.text!, currentPassword: tfPassword.text!){(error) in
+            if error == nil{
+                print("error")
+            }else{
+                print("exito")
+            }
+        }
+    }
+    
     /*
     // MARK: - Navigation
 
